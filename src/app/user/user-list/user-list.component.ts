43491590028348dcb0cae973486c7d7f1a991919
Guste Gaubaite <219.guste@gmail.com>
2025-02-28 11:22:02 +0100

    Create and implement template form to add new member to members collection, use ng classes on inputs to manage field validation with visual feedback

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,21 +1,39 @@
 import { Component } from '@angular/core';
 import { MemberEntity } from '../../model/model';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { HighlightDirective } from '../../directives/highlight.directive';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [NgFor, HighlightDirective],
+  imports: [NgFor, HighlightDirective, FormsModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
 })
 export class UserListComponent {
   members: MemberEntity[] = [];
+  newMember!: MemberEntity;
+
+  add(): void {
+    this.members.push(this.newMember);
+    this.newMember = {
+      id: '',
+      login: '',
+      avatar_url: '',
+    };
+    console.log(this.members);
+  }
 
   ngOnInit(): void {
     fetch('https://api.github.com/orgs/lemoncode/members')
       .then((response) => response.json())
       .then((result) => (this.members = result));
+
+    this.newMember = {
+      id: '',
+      login: '',
+      avatar_url: '',
+    };
   }
 }
